ec6a8325cad139fc8e8c40361823b189882dade9
laraveller <laraveller2021@gmail.com>
2021-11-02 20:58:55 +0100

    migrations created

--- database/migrations/2021_11_01_205939_create_tags_table.php
+++ database/migrations/2021_11_01_205939_create_tags_table.php
@@ -15,7 +15,15 @@ class CreateTagsTable extends Migration
     {
         Schema::create('tags', function (Blueprint $table) {
             $table->id();
+            $table->string('tag_name')->unique();
+            $table->string('slug');
             $table->timestamps();
+        });
+        Schema::create('taggables', function (Blueprint $table) {
+            $table->integer("tag_id");
+            $table->integer("taggable_id");
+            $table->string("taggable_type");
+
         });
     }
 
@@ -26,6 +34,7 @@ class CreateTagsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('tags');
+         Schema::dropIfExists('tags');
+        Schema::dropIfExists('taggables');
     }
 }
